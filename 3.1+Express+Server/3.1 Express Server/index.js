e17f0460d19f846d140a75d19f4fd9470b1fe77d
lahiru imesh <imeshbl.22@uom.lk>
2024-12-16 19:39:00 +0530

    Edit index.js - HTTP Requests

--- 3.1+Express+Server/3.1 Express Server/index.js
+++ 3.1+Express+Server/3.1 Express Server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
--- 3.2+HTTP+Requests/3.2 HTTP Requests/index.js
+++ 3.2+HTTP+Requests/3.2 HTTP Requests/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send("<h1>Hello World !!!</h1>");
 });
 
 app.get("/about", (req, res) => {
@@ -11,9 +11,10 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+  res.send("<h1>Contact Me</h1><h2>Phone: +44123456789</h2><p>Please contact me</p>");
 });
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
