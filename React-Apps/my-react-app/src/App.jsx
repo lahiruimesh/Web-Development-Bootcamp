e352f7a2e15d7dc166a6e7e4fe3573741f4a24f7
lahiru imesh <imeshbl.22@uom.lk>
2025-02-18 13:35:27 +0530

    Add some changes

--- React-Apps/my-react-app/src/App.jsx
+++ React-Apps/my-react-app/src/App.jsx
@@ -1,12 +1,19 @@
+import React from 'react';
 import { useState } from 'react'
 import './App.css'
+import logo from './assets/react.svg'
 
+const a = 12;
+const b = "Kamal";
 function App() {
   return (
     <>
       <div>
-        <h1>Hello World !!!</h1>
+        <h1 className='h'>Hello {b} !!!</h1>
+        <h3>I am {a} years old.</h3>
+        <img src= {logo} alt="logo" />
         <p>This is a paragraph.</p>
+        <p>My lucky number is {Math.floor(Math.random() * 34)}</p>
       </div>
     </>
   )
