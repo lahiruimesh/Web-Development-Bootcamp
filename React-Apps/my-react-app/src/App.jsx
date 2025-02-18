442f962773bfe88949a231194cd4b2556090141e
lahiru imesh <imeshbl.22@uom.lk>
2025-02-18 11:33:52 +0530

    Added new folder - React Apps

---
+++ React-Apps/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello World !!!</h1>
+        <p>This is a paragraph.</p>
+      </div>
+    </>
+  )
+}
+
+export default App;
