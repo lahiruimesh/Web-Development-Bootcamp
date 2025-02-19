70835c5edfa6297e1571fbe434b26c5c631b96fe
lahiru imesh <imeshbl.22@uom.lk>
2025-02-19 17:10:05 +0530

    Create Maths.jsx file in to my-react-app folder

---
+++ React-Apps/my-react-app/src/Maths.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+let x;
+let y;
+
+function Sum(x, y){
+    return x + y;
+};
+
+function Sub(x, y){
+    return x - y;
+};
+
+function Mul(x, y){
+    return x * y;
+};
+
+function Div(x, y){
+    return x / y;
+};
+
+
+
+//export default x;
+
+export {Sum, Sub, Mul, Div};  
