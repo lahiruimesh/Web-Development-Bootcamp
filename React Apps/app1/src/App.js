8a9c74df4c4d50b9c11740ea2843f848ba7c17ba
lahiru imesh <imeshbl.22@uom.lk>
2025-03-08 20:04:09 +0530

    Delete components

--- React Apps/app1/src/App.js
+++ React Apps/app1/src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-import ProductCard1 from "./Components/ProductCard1";
-import ProductCard2 from "./Components/ProductCard2";
-import products from "./Components/Products";
-import Form from "./Pages/NameForm";
-
-
 function App() {
   return (
     <div>
-      <h1>Welcome to the Online Store!</h1>
+      <h1>Welcome ...</h1>
   </div>
   );
 }
